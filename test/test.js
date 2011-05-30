f2754e1894df8b52da0daaf629a8934f19a22e2b
Masatoshi Teruya <mah0x211@gmail.com>
2011-05-31 08:47:44 +0900

    change: format options

--- test/test.js
+++ test/test.js
@@ -1,20 +1,27 @@
-var uuid = require('../lib/ossp_uuid');
+var sys = require('sys'),
+	uuid = require('../lib/ossp_uuid');
 
-console.log( uuid );
-console.log( uuid.version() );
-console.log( 'v1' );
-console.log( uuid.generate( uuid.fmt.STR, uuid.V1 ) );
-console.log( uuid.generate( uuid.fmt.SIV, uuid.V1 ) );
-console.log( uuid.generate( uuid.fmt.TXT, uuid.V1 ) );
-console.log( 'v3' );
-console.log( uuid.generate( uuid.fmt.STR, uuid.V3, uuid.ns.url, 'http://example.com/' ) );
-console.log( uuid.generate( uuid.fmt.SIV, uuid.V3, uuid.ns.url, 'http://example.com/' ) );
-console.log( uuid.generate( uuid.fmt.TXT, uuid.V3, uuid.ns.url, 'http://example.com/' ) );
-console.log( 'v4' );
-console.log( uuid.generate( uuid.fmt.STR, uuid.V4 ) );
-console.log( uuid.generate( uuid.fmt.SIV, uuid.V4 ) );
-console.log( uuid.generate( uuid.fmt.TXT, uuid.V4 ) );
-console.log( 'v5' );
-console.log( uuid.generate( uuid.fmt.STR, uuid.V5, uuid.ns.url, 'http://example.com/' ) );
-console.log( uuid.generate( uuid.fmt.SIV, uuid.V5, uuid.ns.url, 'http://example.com/' ) );
-console.log( uuid.generate( uuid.fmt.TXT, uuid.V5, uuid.ns.url, 'http://example.com/' ) );
+console.log( 
+	"version 1\n" +
+	'  str: ' + uuid.generate( uuid.STR, uuid.V1 ) + "\n" + 
+	'  siv: ' + uuid.generate( uuid.SIV, uuid.V1 ) + "\n" +
+	'  bin: ' + sys.inspect( uuid.generate( uuid.BIN, uuid.V1 ) ) + "\n" +
+	"  txt: \n" + uuid.generate( uuid.TXT, uuid.V1 ) );
+console.log( 
+	"version 3\n" +
+	'  str: ' + uuid.generate( uuid.STR, uuid.V3, uuid.ns.url, 'http://example.com/' ) + "\n" + 
+	'  siv: ' + uuid.generate( uuid.SIV, uuid.V3, uuid.ns.url, 'http://example.com/' ) + "\n" + 
+	'  bin: ' + sys.inspect( uuid.generate( uuid.BIN, uuid.V3, uuid.ns.url, 'http://example.com/' ) ) + "\n" + 
+	'  txt: ' + uuid.generate( uuid.TXT, uuid.V3, uuid.ns.url, 'http://example.com/' ) );
+console.log( 
+	"version 4\n" +
+	'  str: ' + uuid.generate( uuid.STR, uuid.V4 ) + "\n" + 
+	'  siv: ' + uuid.generate( uuid.SIV, uuid.V4 ) + "\n" + 
+	'  bin: ' + sys.inspect( uuid.generate( uuid.BIN, uuid.V4 ) ) + "\n" + 
+	"  txt: \n" + uuid.generate( uuid.TXT, uuid.V4 ) );
+console.log( 
+	"version 5\n" +
+	'  str: ' + uuid.generate( uuid.STR, uuid.V5, uuid.ns.url, 'http://example.com/' ) + "\n" +
+	'  siv: ' + uuid.generate( uuid.SIV, uuid.V5, uuid.ns.url, 'http://example.com/' ) + "\n" + 
+	'  bin: ' + sys.inspect( uuid.generate( uuid.BIN, uuid.V5, uuid.ns.url, 'http://example.com/' ) ) + "\n" + 
+	"  txt: \n" + uuid.generate( uuid.TXT, uuid.V5, uuid.ns.url, 'http://example.com/' ) );
